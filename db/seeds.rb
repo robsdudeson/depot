a547a453dda9a97b1f6b884c6b5de8aa43a0423c
Robby Thompson <robsdudeson@gmail.com>
2021-08-19 21:07:14 -0400

    add Product seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create!(
+  title: 'Build Chatbot Interactions',
+  description:
+  %(<p>
+    <em>Responsive, Intuitive Interfaces with Ruby</em>
+    The next step in the evolution of user interfaces is here.
+    Chatbots let your users interact with your service in their
+    own natural language. Use free and open source tools along
+    with Ruby to build creative, useful, and unexpected interactions
+    for users. Take advantage of the Lita framework’s step-by-step
+    implementation strategy to simplify bot development and testing.
+    From novices to experts, chatbots are an area in which everyone
+    can participate. Exercise your creativity by creating chatbot
+    skills for communicating, information, and fun.
+    </p>),
+  image_url: 'dpchat.jpg',
+  price: 20.00
+)
+
+Product.create!(
+  title: 'Build Chatbot Interactions: The Singularity',
+  description:
+  %(<p>
+    <em>Responsive, Intuitive Interfaces with Ruby</em>
+    The next step in the evolution of user interfaces is here.
+    Chatbots let your users interact with your service in their
+    own natural language. Use free and open source tools along
+    with Ruby to build creative, useful, and unexpected interactions
+    for users. Take advantage of the Lita framework’s step-by-step
+    implementation strategy to simplify bot development and testing.
+    From novices to experts, chatbots are an area in which everyone
+    can participate. Exercise your creativity by creating chatbot
+    skills for communicating, information, and fun.  But then they 
+    take over
+    </p>),
+  image_url: 'dpchat-singularity.jpg',
+  price: 20.00
+)
